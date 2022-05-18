4626eaf4e961eda63d872ffda542f5e66772d621
poggio64.jp <poggio.dg@gmail.com>
2022-05-17 21:26:41 -0300

    estilos

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -15,7 +15,7 @@ function HomeScreen() {
       <section  className='banner' style={{flexDirection: 'column', alignItems: 'center'}}>
       <div style={{flexDirection:'row', alignItems: 'center', marginBottom: 200, justifyContent:'center'}}>
         <h1 style={{ color:'white', textTransform: "uppercase"}}>Tableros de Control</h1>
-        <div style={{width:100, height: 3, backgroundColor:'#ef3742'}}></div>
+        <div style={{width:100, height: 5, backgroundColor:'#ef3742'}}></div>
       </div>
       
       </section>
