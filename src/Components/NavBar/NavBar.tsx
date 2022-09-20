9e2bc5811229d4bb09ca54d82869d9248cab629e
Juan Poggio <jpoggio@buenosaires.gob.ar>
2022-09-19 22:02:22 -0300

    code refactor de la navbar

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,37 +1,48 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route, 
-  Link
-} from "react-router-dom";
-import './NavBar.css';
-import Logo from '../../assets/Logo.png'
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./NavBar.css";
+import Logo from "../../assets/Logo.png";
 
 const rootElement = document.getElementById("root");
 
+var screens = [
+  { name: "home", to: "/Tableros" },
+  { name: "Finanzas", to: "/Finanzas" },
+  {name: "Operaciones", to: "/Contact", subScreens: [{ name: "padentro", to: "/Tableros"}, { name: "pafuera", to: "/Finanzas"}]},
+  {name: "Comercial", to: "/Contact"},
+  {name: "Recursos Humanos",to: "/Contact"}
+];
+
 function NavBar() {
   return (
-    <div className='navBar'>
+    <div className="navBar">
       <div>
-      <img src={Logo} style={{width:114, height: 67, alignSelf: 'center', margin: 20}}/>
+        <img
+          src={Logo}
+          style={{ width: 114, height: 67, alignSelf: "center", margin: 20 }}
+        />
       </div>
-      <div>
-        <Link to="/Tableros" className="element">
-        Home
-        </Link>
-        <Link to="/Finanzas" className="element">
-          Finanzas
-        </Link>
-        <Link to="/Contact" className="element">
-          Operaciones
-        </Link>
-        <Link to="/Contact" className="element">
-          Comercial
-        </Link>
-        <Link to="/Contact" className="element">
-          Recursos Humanos
-        </Link>
+      <div className="elementContainer">
+        {screens.map((e) =>
+          e.subScreens ? (
+            <div className="suBavBarContainer">
+              <Link to={e.to} className="element">
+                {e.name}
+              </Link>
+              <div className="suBavBar">
+                {e.subScreens.map((e) => (
+                  <Link to={e.to} className="element">
+                    {e.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          ) : (
+            <Link to={e.to} className="element">
+              {e.name}
+            </Link>
+          )
+        )}
       </div>
     </div>
   );
